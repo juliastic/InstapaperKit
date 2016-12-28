d5f9bcd6687f57ff399a28daabdb7ee61cdded69
Marcel Voß <me@marcelvoss.com>
2016-12-28 19:25:47 +0100

    Add logic for GET method

--- Demo/Demo/Networking.swift
+++ Demo/Demo/Networking.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class Networking: NSObject {
     
-    class func GET(url: URL?, parameters: [String: String]?, headers: [String: String]? = nil, completionHandler:(Error?, Data?) -> Void) {
-        if let bindedURL = url {
-            var request = URLRequest(url: bindedURL)
-            request.httpMethod = "GET"
-            
-            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
-                
-            })
+    class func GET(url: URL?, parameters: [String: String]?, headers: [String: String]? = nil, completionHandler:@escaping (Error?, [String: Any]?, HTTPURLResponse?) -> Void) {
+        guard url != nil else {
+            completionHandler(nil, nil, nil)
+            return
         }
+        
+        let configuration = URLSessionConfiguration.default
+        
+        var queryURL = url
+        if let parameterDict = parameters {
+            queryURL = encode(url: queryURL!, queries: parameterDict)
+        }
+        
+        var request = URLRequest(url: queryURL!)
+        request.httpMethod = "GET"
+        
+        if let headerDictionary = headers {
+            for header in headerDictionary {
+                request.addValue(header.value, forHTTPHeaderField: header.key)
+            }
+        }
+        
+        let session = URLSession(configuration: configuration)
+        session.dataTask(with: request) { (data, response, error) in
+            let serverResponse = response as? HTTPURLResponse
+            
+            if let receivedData = data {
+                do {
+                    let results = try JSONSerialization.jsonObject(with: receivedData, options: []) as? [String: Any]
+                    completionHandler(nil, results, serverResponse)
+                } catch let error {
+                    completionHandler(error, nil, serverResponse)
+                }
+            }
+            completionHandler(error, nil, serverResponse)
+            
+            }.resume()
     }
     
     class func POST(url: URL?, parameters: [String: String]?, headers: [String: String]? = nil, completionHandler:@escaping (Error?, [String: Any]?, HTTPURLResponse?) -> Void) {
@@ -42,7 +70,6 @@ class Networking: NSObject {
                 request.addValue(header.value, forHTTPHeaderField: header.key)
             }
         }
-        request.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
         
         let session = URLSession(configuration: configuration)
         session.dataTask(with: request) { (data, response, error) in
