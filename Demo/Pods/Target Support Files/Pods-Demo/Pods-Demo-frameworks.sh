8ce53d4a5ff5267ce5a7a21e41e55f0df18fc852
Marcel Voss <me@marcelvoss.com>
2016-12-31 15:18:44 +0100

    Add ‘ReachabilitySwift’ via CocoaPods

--- Demo/Pods/Target Support Files/Pods-Demo/Pods-Demo-frameworks.sh
+++ Demo/Pods/Target Support Files/Pods-Demo/Pods-Demo-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ReachabilitySwift/ReachabilitySwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftKeychainWrapper/SwiftKeychainWrapper.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ReachabilitySwift/ReachabilitySwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftKeychainWrapper/SwiftKeychainWrapper.framework"
 fi
