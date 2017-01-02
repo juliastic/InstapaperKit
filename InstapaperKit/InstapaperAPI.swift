7cab555cb2416490143b35a2adc8282c78c27a71
Marcel Voss <me@marcelvoss.com>
2017-01-02 22:43:33 +0100

    Make a couple of changes

--- InstapaperKit/InstapaperAPI.swift
+++ InstapaperKit/InstapaperAPI.swift
@@ -1,18 +1,34 @@
 //
-//  Instapaper.swift
-//  Demo
+// Instapaper.swift
 //
-//  Created by Julia Grill on 29/12/2016.
-//  Copyright © 2016 Marcel Voss. All rights reserved.
+// Copyright (c) 2016 Julia Grill and Marcel Voss
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 import UIKit
 import SwiftKeychainWrapper
 import ReachabilitySwift
 
 
-/// Possible response errors from Instapaper's servers
-/// This enum provides custom errors when connecting with Instapaper's servers
+/// Possible response errors from Instapaper's servers.
+/// This enum provides custom errors when connecting with Instapaper's servers.
 ///
 /// - ConnectionInvalid: Invalid account information
 /// - ConnectionTimedOut: Connection to Instapaper's servers timed out
@@ -35,12 +51,21 @@ class InstapaperAPI: NSObject {
     static private let instapaperURL = "https://www.instapaper.com"
     static private let queuedURLsKey = "queuedURLs"
     static private var queuedURLs = [URLInfo]()
-
-    static private let instapaperAPIURL = URL(string: "https://www.instapaper.com/api/")
-    static private let authenticate = "authenticate"
-    static private let add = "add"
+    static private let baseURL = URL(string: "https://www.instapaper.com/api/")
     
-    static private var username: String?
+    /// Computed property that sets and retrieves usernames from the device's keychain.
+    static private var username: String? {
+        get {
+            return defaults.string(forKey: "kInstapaperUsername")
+        } set {
+            if let username = newValue {
+                defaults.set(username, forKey: "kInstapaperUsername")
+            }
+            
+        }
+    }
+    
+    /// Computed property that sets and retrieves passwords from the device's keychain.
     static private var password: String? {
         get {
             return KeychainWrapper.standard.string(forKey: "password")
@@ -54,13 +79,13 @@ class InstapaperAPI: NSObject {
     /// Logs user into Instapaper
     ///
     /// - Parameters:
-    ///   - username: username of account
-    ///   - password: password of account, may be empty
-    ///   - closure: if succesfully logged in closure's parameters are set to true, nil, if error occured parameters are set to false, ResponseError
+    ///   - username: Username of account
+    ///   - password: Password of account, may be empty
+    ///   - closure: If succesfully logged in closure's parameters are set to true, nil, if error occured parameters are set to false, ResponseError
     class func logIn(_ username: String, withPassword password: String, closure: @escaping (_ authorized: Bool, _ error: Error?) -> Void) {
         if self.username == nil {
             let parameters = ["username": username.trimmingCharacters(in: .whitespacesAndNewlines), "password": password.trimmingCharacters(in: .whitespacesAndNewlines)]
-            Networking.GET(url: instapaperAPIURL?.appendingPathComponent(authenticate), parameters: parameters) { (error, result, response) in
+            Networking.GET(url: baseURL?.appendingPathComponent("authenticate"), parameters: parameters) { (error, result, response) in
                 if let response = response {
                     switch response.statusCode {
                     case 200:
@@ -83,13 +108,13 @@ class InstapaperAPI: NSObject {
         }
     }
     
-    /// Adds url to Instapaper account
+    /// Adds given URL to stored Instapaper account
     ///
     /// - Parameters:
-    ///   - url: url to be added
-    ///   - title: title of url
-    ///   - selection: selection of url
-    ///   - closure: if succesfully added closure's parameters are set to true, nil, if error occured parameters are set to false, ResponseError
+    ///   - url: URL to be added
+    ///   - title: Title of URL
+    ///   - selection: Selection of URL
+    ///   - closure: If succesfully added closure's parameters are set to true, nil, if error occured parameters are set to false, ResponseError
     class func add(_ url: URL, withTitle title: String?, selection: String?, closure: @escaping (_ sent: Bool, _ error: Error?) -> Void) {
         if let username = self.username {
             let parameters = ["username": username, "password": password ?? "", "url": url.absoluteString, "title": title ?? "", "selection": selection ?? ""] as [String : String]
@@ -110,7 +135,7 @@ class InstapaperAPI: NSObject {
                 }
             }
             
-            Networking.POST(url: instapaperAPIURL?.appendingPathComponent(add), parameters: parameters) { (error, result, response) in
+            Networking.POST(url: baseURL?.appendingPathComponent("add"), parameters: parameters) { (error, result, response) in
                 if let response = response {
                     switch response.statusCode {
                     case 201:
@@ -129,7 +154,9 @@ class InstapaperAPI: NSObject {
         }
     }
     
-    /// queues a URLInfo object to queuedURLs
+    // MARK: - Private
+    
+    /// Queues a URLInfo object to queuedURLs
     ///
     /// - Parameter info: URLInfo to be added
     private class func queueURLInfo(_ info: URLInfo) {
@@ -141,7 +168,7 @@ class InstapaperAPI: NSObject {
         }
     }
     
-    /// deques a URLInfo object from queuedURLs
+    /// Deques a URLInfo object from queuedURLs
     ///
     /// - Parameter info: URLInfo to be removed
     private class func dequeURL(_ info: URLInfo) {
